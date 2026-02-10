09dceb626b738bb90afbb5a93321759735ce66e0
Isaac J <88879000+fnfplayer64@users.noreply.github.com>
2026-02-11 10:31:05 +1100

    wip: walls, mvement and collisioin

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -33,4 +33,9 @@ public class PlayerController : MonoBehaviour
 
         rb.AddForce(movement * speed); 
     }
+
+     void OnTriggerEnter(Collider other) 
+   {
+   other.gameObject.SetActive(false);
+   }
 }
