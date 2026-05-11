6f9ff31edb0156ffac81fab1b3c7b23f6453570f
Isaac J <88879000+fnfplayer64@users.noreply.github.com>
2026-05-11 19:41:14 +1000

    revamp

--- Assets/scripts/playercontroller.cs
+++ Assets/scripts/playercontroller.cs
@@ -1,43 +1,74 @@
-using System.Collections;
-using System.Collections.Generic;
-using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using TMPro;
 
 public class PlayerController : MonoBehaviour
 {
- // Rigidbody of the player.
- private Rigidbody rb; 
+    public float speed = 10f; 
+    public TextMeshProUGUI countText;
+    public GameObject winTextObject;
 
- // Movement along X and Y axes.
- private float movementX;
- private float movementY;
+    private Rigidbody rb; 
+    private int count;
+    private float movementX;
+    private float movementY;
 
- // Speed at which the player moves.
- public float speed = 0; 
-
- // Start is called before the first frame update.
- void Start()
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
     {
- // Get and store the Rigidbody component attached to the player.
-        rb = GetComponent<Rigidbody>();
+        rb = GetComponent<Rigidbody>(); 
+        count = 0; 
+
+        SetCountText();
+        winTextObject.SetActive(false);
     }
- 
- // This function is called when a move input is detected.
- void OnMove(InputValue movementValue)
-    {
- // Convert the input value into a Vector2 for movement.
-        Vector2 movementVector = movementValue.Get<Vector2>();
 
- // Store the X and Y components of the movement.
+   void OnMove(InputValue movementValue)
+   {
+        Vector2 movementVector = movementValue.Get<Vector2>(); 
+
         movementX = movementVector.x; 
         movementY = movementVector.y; 
-    }
+   }
 
-void FixedUpdate()
-{
-    Vector3 movement = new Vector3(movementX, 0f, movementY);
-    rb.AddForce(movement * speed);
-}
+      void SetCountText() 
+   {
+       countText.text =  "Count: " + count.ToString();
 
+       if(count >=20) 
+       {
+            winTextObject.SetActive(true);
+            Destroy(GameObject.FindGameObjectWithTag("Enemy"));
+            Destroy(GameObject.FindGameObjectWithTag("EnemyWall"));
+       }
+   }
+
+      void FixedUpdate() 
+   {
+        Vector3 movement = new Vector3(movementX, 0.0f, movementY);
+
+       rb.AddForce(movement * speed); 
+   }
+
+   void OnTriggerEnter (Collider other) 
+   {
+       if (other.gameObject.CompareTag("PickUp")) 
+       {
+           other.gameObject.SetActive(false);
+           count = count + 1;
+
+           SetCountText();
+       }
+
+   }
+
+   private void OnCollisionEnter(Collision collision)
+   {
+        if (collision.gameObject.CompareTag("Enemy") || collision.gameObject.CompareTag("EnemyWall"))
+        {
+            Destroy(gameObject);
+            winTextObject.gameObject.SetActive(true);
+            winTextObject.GetComponent<TextMeshProUGUI>().text = "You Lose!";
+        }
+   }
 }
